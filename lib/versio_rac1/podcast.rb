0312900150398f48d97de6a32fd7377584218164
Josep <apuratepp@gmail.com>
2015-08-07 21:53:32 +0200

    Initial commit

---
+++ lib/versio_rac1/podcast.rb
@@ -0,0 +1,7 @@
+require "versio_rac1/podcast/version"
+
+module VersioRac1
+  module Podcast
+    # Your code goes here...
+  end
+end
---
+++ lib/versio_rac1/podcast/version.rb
@@ -0,0 +1,5 @@
+module VersioRac1
+  module Podcast
+    VERSION = "0.1.0"
+  end
+end
