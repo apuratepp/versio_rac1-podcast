b10cde179f826a89114ba0385ab962c5f380d2fb
Josep <apuratepp@gmail.com>
2015-08-09 15:49:45 +0200

    Episode.all and Episode.find

--- lib/versio_rac1/podcast/episode.rb
+++ lib/versio_rac1/podcast/episode.rb
@@ -9,6 +9,17 @@ module VersioRac1
       attribute :broadcasted_at
       attribute :link
 
+      def self.all
+        Client.list
+      end
+
+      def self.find(date)
+        all.select do |episode|
+          episode.broadcasted_at > date &&
+          episode.broadcasted_at < date + (24*3600)
+        end
+      end
+
       def self.from_slop(episode_slop)
         title = episode_slop.title.text
         broadcasted_at = parse_description(episode_slop.at("description").text)
--- spec/versio_rac1/podcast/episode_spec.rb
+++ spec/versio_rac1/podcast/episode_spec.rb
@@ -4,6 +4,32 @@ describe VersioRac1::Podcast::Episode do
   let(:description) { "Emis.: 07-08-15 a les 18h" }
   let(:parsed)      { Time.utc(2015, 8, 7, 16) }
 
+  describe ".all" do
+    let(:list) { double("list") }
+
+    before do
+      allow(VersioRac1::Podcast::Client).to receive(:list).and_return(list)
+    end
+
+    subject { described_class.all }
+    it      { is_expected.to eq(list) }
+  end
+
+  describe ".find" do
+    let(:date) { Time.local(2015,7,1) }
+    let(:episode1) { double("episode", broadcasted_at: Time.local(2015,6,30,18)) }
+    let(:episode2) { double("episode", broadcasted_at: Time.local(2015,7,1,18)) }
+    let(:episode3) { double("episode", broadcasted_at: Time.local(2015,7,2,18)) }
+    let(:all_espisodes) { [episode1, episode2, episode3] }
+
+    before do
+      allow(described_class).to receive(:all).and_return(all_espisodes)
+    end
+
+    subject { described_class.find(date) }
+    it      { is_expected.to eq([episode2]) }
+  end
+
   describe ".parse_description" do
     subject { described_class.parse_description(description) }
     it      { is_expected.to eq(parsed) }
